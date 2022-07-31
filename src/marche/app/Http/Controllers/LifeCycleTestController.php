d8c28f76eabf7c2409603eda3ca0795eb580361e
iwafuchi <rta375@gmail.com>
2022-07-31 23:08:30 +0900

    feat: Practice service container

--- src/marche/app/Http/Controllers/LifeCycleTestController.php
+++ src/marche/app/Http/Controllers/LifeCycleTestController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\Sample;
+use App\Http\Controllers\Message;
 
 class LifeCycleTestController extends Controller {
     //
@@ -11,8 +13,23 @@ class LifeCycleTestController extends Controller {
             return 'ライフサイクルテスト';
         });
 
+        /**
+         * サービスコンテナなしのパターン
+         * SampleClassを
+         */
+        $sample1 = new Sample(new Message());
+        $sample1->run('サービスコンテナを使用していません。');
+
+        /**
+         * サービスコンテナapp()ありのパターン
+         * SampleClassはMessageClassに依存していたがサービスコンテナapp()を使用することで自動的に依存関係を解消してくれている。
+         */
+        app()->bind('sample', Sample::class);
+        $sample2 = app()->make('sample');
+        $sample2->run("サービスコンテナを使用しています。");
+
         $make = app()->make('lifeCycleTest');
 
-        dd($make, app());
+        dd($make, $sample2, app());
     }
 }
