000407366c1ab7ae441c10d0c101f6642093b5d6
iwafuchi <rta375@gmail.com>
2022-08-06 00:43:51 +0900

    feat: Add Test model and tests tables

--- src/marche/database/migrations/2022_08_05_125724_create_tests_table.php
+++ src/marche/database/migrations/2022_08_05_125724_create_tests_table.php
@@ -4,17 +4,20 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
-    {
+    public function up() {
         Schema::create('tests', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -24,8 +27,7 @@ return new class extends Migration
      *
      * @return void
      */
-    public function down()
-    {
+    public function down() {
         Schema::dropIfExists('tests');
     }
 };
