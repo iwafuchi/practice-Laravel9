382650465a61c1415c4ddb3616d1b738f8dda03f
iwafuchi <rta375@gmail.com>
2022-09-04 03:58:29 +0900

    feat: Add function of upload image without resizing

--- src/marche/app/Http/Controllers/Owners/ShopController.php
+++ src/marche/app/Http/Controllers/Owners/ShopController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Owners;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 use App\Models\Shop;
 
 
@@ -32,9 +33,17 @@ class ShopController extends Controller {
     }
 
     public function edit($id) {
-        dd(Shop::findOrFail($id));
+        $shop = Shop::findOrFail($id);
+        return view('owners.shops.edit', compact('shop'));
     }
 
+    //upload image
     public function update(Request $request, $id) {
+        $imageFile = $request->image;
+        if (!is_null($imageFile) && $imageFile->isValid()) {
+            Storage::putFile('public/shops', $imageFile);
+            dd($imageFile);
+        }
+        return redirect()->route('owners.shops.index');
     }
 }
