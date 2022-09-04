f57506906508a906e8f15fe84f7a709817b3017d
iwafuchi <rta375@gmail.com>
2022-09-04 17:42:30 +0900

    feat: Add image upload and resizing functionality using Intervention/Image

--- src/marche/app/Http/Controllers/Owners/ShopController.php
+++ src/marche/app/Http/Controllers/Owners/ShopController.php
@@ -41,12 +41,10 @@ class ShopController extends Controller {
     public function update(Request $request, $id) {
         $imageFile = $request->image;
         if (!is_null($imageFile) && $imageFile->isValid()) {
-            // Storage::putFile('public/shops', $imageFile);
             $fileName = uniqid(rand() . '');
             $extension = $imageFile->extension();
             $fileNameToStore = $fileName . '.' . $extension;
             $resizedImage = InterventionImage::make($imageFile)->resize(1920, 1080)->encode();
-            dd($imageFile, $resizedImage);
             Storage::put('public/shops/' . $fileNameToStore, $resizedImage);
         }
         return redirect()->route('owners.shops.index');
