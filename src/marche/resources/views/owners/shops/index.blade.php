90b9ef8867cf0d73f5038fa4acde3d3162961f4d
iwafuchi <rta375@gmail.com>
2022-09-05 22:29:48 +0900

    feat: Update shops edit

--- src/marche/resources/views/owners/shops/index.blade.php
+++ src/marche/resources/views/owners/shops/index.blade.php
@@ -20,16 +20,8 @@
                                             <span class="border p-2 rounded-md bg-red-400 text-white">販売停止中</span>
                                         @endif
                                     </div>
-                                    <div class="text-xl">
-                                        {{ $shop->name }}
-                                    </div>
-                                    <div>
-                                        @if (empty($shop->filename))
-                                            <img src="{{ asset('images/no_image.jpg') }}">
-                                        @else
-                                            <img src="{{ asset('storage/shops/' . $shop->filename) }}">
-                                        @endif
-                                    </div>
+                                    <div class="text-xl">{{ $shop->name }}</div>
+                                    <x-shop-thumbnail />
                                 </div>
                             </a>
                         </div>
