15b75a664ff50610e44f8ee3ef6c68bd42e19ea5
iwafuchi <rta375@gmail.com>
2022-09-08 14:11:37 +0900

    feat: Create ImageSeeder

---
+++ src/marche/database/seeders/ImageSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ImageSeeder extends Seeder {
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run() {
+        for ($i = 1; $i < 7; $i++) {
+            DB::table('images')->insert([
+                [
+                    'owner_id' => $i,
+                    'filename' => "sample${i}.jpg",
+                    'title' => null,
+                    'created_at' => '2022/01/01 00:00:00'
+                ]
+            ]);
+        }
+    }
+}
