14d3973541ad271e756a19d251fa44d598af5f2f
iwafuchi <rta375@gmail.com>
2022-09-08 14:15:16 +0900

    feat : Refactorign

--- src/marche/database/seeders/OwnerSeeder.php
+++ src/marche/database/seeders/OwnerSeeder.php
@@ -13,43 +13,15 @@ class OwnerSeeder extends Seeder {
      * @return void
      */
     public function run() {
-        DB::table('owners')->insert([
-            [
-                'name' => 'test1',
-                'email' => 'test1@test.com',
-                'password' => Hash::make('password123'),
-                'created_at' => '2022/01/01 00:00:00'
-            ],
-            [
-                'name' => 'test2',
-                'email' => 'test2@test.com',
-                'password' => Hash::make('password123'),
-                'created_at' => '2022/01/01 00:00:00'
-            ],
-            [
-                'name' => 'test3',
-                'email' => 'test3@test.com',
-                'password' => Hash::make('password123'),
-                'created_at' => '2022/01/01 00:00:00'
-            ],
-            [
-                'name' => 'test4',
-                'email' => 'test4@test.com',
-                'password' => Hash::make('password123'),
-                'created_at' => '2022/01/01 00:00:00'
-            ],
-            [
-                'name' => 'test5',
-                'email' => 'test5@test.com',
-                'password' => Hash::make('password123'),
-                'created_at' => '2022/01/01 00:00:00'
-            ],
-            [
-                'name' => 'test6',
-                'email' => 'test6@test.com',
-                'password' => Hash::make('password123'),
-                'created_at' => '2022/01/01 00:00:00'
-            ],
-        ]);
+        for ($i = 1; $i < 7; $i++) {
+            DB::table('owners')->insert([
+                [
+                    'name' => "test{$i}",
+                    'email' => "test${i}@test.com",
+                    'password' => Hash::make('password123'),
+                    'created_at' => '2022/01/01 00:00:00'
+                ]
+            ]);
+        }
     }
 }
