9537e5d8a245ae5abfe2333d3f058a6f2dac9231
iwafuchi <rta375@gmail.com>
2022-09-14 18:03:37 +0900

    feat:Setting micromodal.js

---
+++ src/marche/public/js/view-image.js
@@ -0,0 +1,20 @@
+'use strict'
+
+function viewImage() {
+    const images = document.querySelectorAll('.image') //全てのimageタグを取得
+    images.forEach(image => {
+        image.addEventLister('click', function(e) {
+            const imageName = e.target.dataset.id.substr(0, 6);
+            const imageId = e.target.dataset.id.replace(imageName + '_', '');
+            const imageFile = e.target.dataset.file;
+            const imagePath = e.target.dataset.path;
+            const modal = e.target.dataset.modal;
+
+            document.getElementById(imageName + '_thumbnail').src = imagePath + '/' + imageFile;
+            document.getElementById(imageName + '_hidden').value = imageId;
+            Micromodal.close(modal);
+        })
+    });
+}
+
+// viewImage();
