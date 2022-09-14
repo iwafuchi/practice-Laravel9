da38491aed6014b536aa5f37cc975f2d16d836d7
iwafuchi <rta375@gmail.com>
2022-09-14 22:04:08 +0900

    feat: Changed to split javascript code into files and load them.

--- src/marche/resources/views/owners/products/create.blade.php
+++ src/marche/resources/views/owners/products/create.blade.php
@@ -43,21 +43,5 @@
             </div>
         </div>
     </div>
-    <script>
-        'use strict'
-        const images = document.querySelectorAll('.image')
-
-        images.forEach(image => {
-            image.addEventListener('click', function(e) {
-                const imageName = e.target.dataset.id.substr(0, 6)
-                const imageId = e.target.dataset.id.replace(imageName + '_', '')
-                const imageFile = e.target.dataset.file
-                const imagePath = e.target.dataset.path
-                const modal = e.target.dataset.modal
-                document.getElementById(imageName + '_thumbnail').src = imagePath + '/' + imageFile
-                document.getElementById(imageName + '_hidden').value = imageId
-                MicroModal.close(modal);
-            }, )
-        })
-    </script>
+    <script src="{{ asset('/js/view-image.js') }}"></script>
 </x-app-layout>
