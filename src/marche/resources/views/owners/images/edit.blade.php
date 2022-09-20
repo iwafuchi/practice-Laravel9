4be52ae5aec0ead83afc94d2117f2cc0ad969781
iwafuchi <rta375@gmail.com>
2022-09-20 15:50:02 +0900

    feat: Update layouts

--- src/marche/resources/views/owners/images/edit.blade.php
+++ src/marche/resources/views/owners/images/edit.blade.php
@@ -13,26 +13,30 @@
                     <form method="POST" action="{{ route('owners.images.update', ['image' => $image->id]) }}">
                         @csrf
                         @method('put')
-                        <div class="-m-2">
-                            <div class="p-2 w-1/2 mx-auto">
-                                <div class="relative">
-                                    <label for="title" class="leading-7 text-sm text-gray-600">画像タイトル</label>
-                                    <input type="text" id="title" name="title" value="{{ $image->title }}"
-                                        class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-purple-500 focus:bg-white focus:ring-2 focus:ring-purple-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                        <div class="p-2 w-1/2 mx-auto">
+                            <div class="relative">
+                                <label for="title" class="leading-7 text-sm text-gray-600">画像タイトル</label>
+                                <input type="text" id="title" name="title" value="{{ $image->title }}"
+                                    class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-purple-500 focus:bg-white focus:ring-2 focus:ring-purple-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                            </div>
+                        </div>
+                        <div class="p-2 w-1/2 mx-auto">
+                            <div class="relative">
+                                <div class="w-32">
+                                    <x-thumbnail :filename="$image->filename" type="products" />
                                 </div>
                             </div>
-                            <div class="p-2 w-1/2 mx-auto">
-                                <div class="relative">
-                                    <div class="w-32">
-                                        <x-thumbnail :filename="$image->filename" type="products" />
-                                    </div>
+                        </div>
+                        <div class="flex flex-col">
+                            <div class="flex justify-around">
+                                <div class="p-3 w-1/2 flex justify-around">
+                                    <button onclick="location.href='{{ route('owners.images.index') }}'" type="button"
+                                        class="mx-auto text-gray-50 bg-gray-500 border-0 py-2 px-8 focus:outline-none hover:bg-gray-600 rounded text-lg">戻る</button>
                                 </div>
-                            </div>
-                            <div class="p-2 w-full mt-4 flex justify-around">
-                                <button onclick="location.href='{{ route('owners.images.index') }}'" type="button"
-                                    class="flex mx-auto text-gray-50 bg-gray-500 border-0 py-2 px-8 focus:outline-none hover:bg-gray-600 rounded text-lg">戻る</button>
-                                <button type="submit"
-                                    class="flex mx-auto text-white bg-purple-500 border-0 py-2 px-8 focus:outline-none hover:bg-purple-600 rounded text-lg">更新</button>
+                                <div class="p-3 w-1/2 flex justify-around">
+                                    <button type="submit"
+                                        class="mx-auto text-white bg-purple-500 border-0 py-2 px-8 focus:outline-none hover:bg-purple-600 rounded text-lg">更新</button>
+                                </div>
                             </div>
                         </div>
                     </form>
@@ -40,14 +44,21 @@
                         action="{{ route('owners.images.destroy', ['image' => $image->id]) }}">
                         @csrf
                         @method('delete')
-                        <div class="p-2 w-full mt-4 flex justify-around">
-                            <button type="button" data-id="{{ $image->id }}" onclick="deletePost(this)"
-                                class="flex mx-auto text-white bg-red-500 border-0 py-2 md:px-6 focus:outline-none hover:bg-red-600 rounded ">削除</button>
+                        <div class="flex flex-col">
+                            <div class="flex justify-around">
+                                <div class="p-3 w-1/2">
+                                </div>
+                                <div class="p-3 w-1/2 flex justify-around">
+                                    <button type="button" data-id="{{ $image->id }}" onclick="deletePost(this)"
+                                        class="mx-auto text-white bg-red-500 border-0 py-2 px-8 focus:outline-none hover:bg-red-600 rounded text-lg">削除</button>
+                                </div>
+                            </div>
                         </div>
                     </form>
                 </div>
             </div>
         </div>
+    </div>
     </div>
     <script src="{{ asset('/js/delete-alert.js') }}"></script>
 </x-app-layout>
