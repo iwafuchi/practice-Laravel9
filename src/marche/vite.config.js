a9b4d95fa8d3366485fd62a6f787b4256653c3c2
iwafuchi <rta375@gmail.com>
2022-09-24 23:26:26 +0900

    feat: Add swiper.js

--- src/marche/vite.config.js
+++ src/marche/vite.config.js
@@ -7,8 +7,9 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
-                'resources/js/asset/micromodal/view-image.js',
-                'resources/js/asset/delete-alert.js',
+                'resources/js/asset/modal/view-image.js',
+                'resources/js/asset/alert/delete-alert.js',
+                'resources/js/asset/swiper/swiper.js'
             ],
             refresh: true,
         }),
