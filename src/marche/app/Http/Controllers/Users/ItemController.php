ed03f8eddb6d80803ab2c2bb1dd36a303753246e
iwafuchi <rta375@gmail.com>
2022-09-30 09:42:31 +0900

    feat: Add local scope

--- src/marche/app/Http/Controllers/Users/ItemController.php
+++ src/marche/app/Http/Controllers/Users/ItemController.php
@@ -5,8 +5,6 @@ namespace App\Http\Controllers\Users;
 use App\Http\Controllers\Controller;
 use App\Models\Product;
 use App\Models\Stock;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 
 class ItemController extends Controller {
     public function __construct() {
@@ -18,63 +16,9 @@ class ItemController extends Controller {
      * @return \Illuminate\Http\Response
      */
     public function index() {
-
-        $stocks = DB::table('t_stocks')
-            ->select(
-                'product_id',
-                DB::raw('sum(quantity) as quantity')
-            )
-            ->groupBy('product_id')
-            ->having('quantity', '>=', 1);
-
-        $products = DB::table('products')
-            ->joinSub($stocks, 'stock', function ($join) {
-                $join->on('products.id', '=', 'stock.product_id');
-            })
-            ->join('shops', 'products.shop_id', '=', 'shops.id')
-            ->join(
-                'secondary_categories',
-                'products.secondary_category_id',
-                '=',
-                'secondary_categories.id'
-            )
-            ->join('images as image1', 'products.image1', '=', 'image1.id')
-            ->join('images as image2', 'products.image2', '=', 'image2.id')
-            ->join('images as image3', 'products.image3', '=', 'image3.id')
-            ->join('images as image4', 'products.image4', '=', 'image4.id')
-            ->where('shops.is_selling', true)
-            ->where('products.is_selling', true)
-            ->select(
-                'products.id as id',
-                'products.name as name',
-                'products.price',
-                'products.sort_order as sort_order',
-                'products.information',
-                'secondary_categories.name as category',
-                'image1.filename as filename'
-            )
-            ->get();
+        $products = Product::availableItems()->get();
 
         return view('users.index', compact('products'));
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create() {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request) {
-        //
     }
 
     /**
@@ -91,36 +35,5 @@ class ItemController extends Controller {
         }
 
         return view('users.show', compact('product', 'quantity'));
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id) {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id) {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id) {
-        //
     }
 }
