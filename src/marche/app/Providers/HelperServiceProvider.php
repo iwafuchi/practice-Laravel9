53ed2feba0883e031c02ed35b66d7c6452a0dc2e
iwafuchi <rta375@gmail.com>
2022-10-03 17:02:24 +0900

    feat: Create file

---
+++ src/marche/app/Providers/HelperServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class HelperServiceProvider extends ServiceProvider {
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register() {
+        app()->bind('extractKeywords', function ($app, $input) {
+            return myExtractKeywords($input['keyword']);
+        });
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot() {
+        //
+    }
+}
