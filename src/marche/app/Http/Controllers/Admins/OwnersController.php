4df6d4f769495ed7019ce875973ec9a09163265b
iwafuchi <rta375@gmail.com>
2022-10-04 17:49:34 +0900

    feat: Separation validation in store method

--- src/marche/app/Http/Controllers/Admins/OwnersController.php
+++ src/marche/app/Http/Controllers/Admins/OwnersController.php
@@ -4,10 +4,10 @@ namespace App\Http\Controllers\Admins;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rules;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\DB;
-use App\Http\Requests\OwnerRegisterRequest;
 use App\Models\Owner;
 use App\Models\Shop;
 use Exception;
@@ -41,7 +41,13 @@ class OwnersController extends Controller {
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(OwnerRegisterRequest $request) {
+    public function store(Request $request) {
+
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:owners'],
+            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+        ]);
 
         try {
             DB::transaction(function () use ($request) {
@@ -70,6 +76,16 @@ class OwnersController extends Controller {
                 'message' => 'オーナー登録を実施しました',
                 'status' => 'info'
             ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id) {
+        //
     }
 
     /**
