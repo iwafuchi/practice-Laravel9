2e6d8cf88df932ba9b46feff6501b4ba91a87f8f
iwafuchi <rta375@gmail.com>
2022-10-08 00:00:08 +0900

    feat: Remove unnecessary elseif

--- src/marche/resources/views/layouts/app.blade.php
+++ src/marche/resources/views/layouts/app.blade.php
@@ -21,8 +21,6 @@
             @include('layouts.admins-navigation')
         @elseif(auth('owners')->user())
             @include('layouts.owners-navigation')
-        @elseif(auth('tests')->user())
-            @include('layouts.tests-navigation')
         @elseif(auth('users')->user())
             @include('layouts.users-navigation')
         @endif
